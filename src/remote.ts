377ecf730bcc4305d2dc1c2afebfbc371ea8e0a4
Jason Cheatham <j.cheatham@gmail.com>
2017-02-02 10:19:29 -0500

    Remote executor updates
    - Require a channel for construction
    - Construct the channel as soon as possible in remote.ts
    
    references #697
    
    [ci skip]

--- src/remote.ts
+++ src/remote.ts
@@ -3,8 +3,6 @@ import Remote, { Config } from './lib/executors/Remote';
 import initialize from './intern';
 import Channel from './lib/WebSocketChannel';
 
-initialize(Remote);
-
 declare let intern: Remote;
 
 export interface RemoteParams extends Config {
@@ -18,52 +16,47 @@ export interface RemoteParams extends Config {
 	suites: string[];
 }
 
-const config = <Config>{};
 let suites: string[] = [];
 let loaderScript: string;
 let runInSync: boolean;
-let socketPort: number;
 
 const params: RemoteParams = <any>getQueryParams();
-
-Object.keys(params).forEach((name: keyof RemoteParams) => {
-	const value = params[name];
-
-	// Filter out parameeters than the executor won't understand
-	switch (name) {
-		case 'suites':
-			suites.push(<string>value);
-			break;
-		case 'loaderScript':
-			loaderScript = <string>value;
-			break;
-		case 'runInSync':
-			runInSync = <boolean>value;
-			break;
-		case 'socketPort':
-			socketPort = <number>value;
-			break;
-
-		default:
-			config[name] = value;
-	}
-});
-
-const channel = new Channel({ url: config.basePath, sessionId: config.sessionId, port: socketPort });
-const sendError = (error: Error) => channel.sendMessage('error', error);
-
-config.channel = channel;
+const config: Config = {
+	channel: new Channel({ url: params.basePath, sessionId: params.sessionId, port: params.socketPort })
+};
+const sendError = (error: Error) => config.channel.sendMessage('error', error);
 
 try {
-	intern.configure(config);
+	Object.keys(params).forEach((name: keyof RemoteParams) => {
+		const value = params[name];
 
-	intern.debug(`Query: ${location.search}`);
+		// Filter out parameeters than the executor won't understand
+		switch (name) {
+			case 'suites':
+				suites.push(<string>value);
+				break;
+			case 'loaderScript':
+				loaderScript = <string>value;
+				break;
+			case 'runInSync':
+				runInSync = <boolean>value;
+				break;
+			case 'socketPort':
+				break;
+
+			default:
+				config[name] = value;
+		}
+	});
+
+	initialize(Remote, config);
+
 	intern.debug(`Params: ${JSON.stringify(params)}`);
 
 	// Forward all executor events back to the host Intern
 	intern.on('*', data => {
 		intern.debug(data);
-		let promise = channel.sendMessage(data.name, data.data).catch(console.error);
+		let promise = intern.channel.sendMessage(data.name, data.data).catch(console.error);
 		if (runInSync) {
 			return promise;
 		}
