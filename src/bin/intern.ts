b7f826852f651f0c45572257087bedd988bb739b
Jason Cheatham <j.cheatham@gmail.com>
2017-03-23 18:53:34 -0400

    Grunt task updates
    - Update grunt task to use intern config format
    - Extract much of the logic from src/bin/intern into
      src/lib/node/runner -- this is now shared between the runner and the
      grunt task
    
    references #697
    
    [ci skip]

--- src/bin/intern.ts
+++ src/bin/intern.ts
@@ -1,34 +1,10 @@
 #!/usr/bin/env node
 
-import Node from '../lib/executors/Node';
-import WebDriver from '../lib/executors/WebDriver';
-import { getConfig, projectRequire } from '../lib/node/util';
-import { getLoaderScript } from '../lib/util';
+import runner from '../lib/node/runner';
+import { getConfig } from '../lib/node/util';
 
 getConfig().then(rawConfig => {
-	const isWebDriver = rawConfig.webdriver;
-
-	if (isWebDriver) {
-		WebDriver.initialize(rawConfig);
-	}
-	else {
-		Node.initialize(rawConfig);
-	}
-
-	const config = intern.config;
-	const loader = getLoaderScript(config);
-
-	if (loader) {
-		projectRequire(loader);
-	}
-	else if (isWebDriver) {
-		config.functionalSuites.forEach(projectRequire);
-	}
-	else {
-		config.suites.forEach(projectRequire);
-	}
-
-	return intern.run();
+	return runner(rawConfig);
 }).catch(error => {
 	console.error(error);
 	process.exitCode = 1;
